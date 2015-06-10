c7589827a468aed26eac1f4a47ff04a0baf8e309
Christopher Davis <cdavis9999@gmail.com>
2015-06-10 09:44:58 -0400

    Introduce the Router to the Code Base

---
+++ src/Router/SimpleRouter.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of PMG\Queue
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace PMG\Queue\Router;
+
+use PMG\Queue\Message;
+
+/**
+ * A router implementation that maps message class names to Queue names.
+ *
+ * @since   2.0
+ */
+final class SimpleRouter implements Router
+{
+    /**
+     * The map of class name => queue name values.
+     *
+     * @var array
+     */
+    private $map;
+
+    public function __construct(array $map)
+    {
+        $this->map = $map;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function queueFor(Message $message)
+    {
+        $cls = get_class($message);
+        return isset($this->map[$cls]) ? $this->map[$cls] : null;
+    }
+}
---
+++ test/unit/UnitTestCase.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of PMG\Queue
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace PMG\Queue;
+
+abstract class UnitTestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
