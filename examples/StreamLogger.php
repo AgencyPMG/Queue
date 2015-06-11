adb916eb19bb889597be5d75b59d4a8ffe12d1e5
Christopher Davis <cdavis9999@gmail.com>
2015-06-11 12:17:58 -0500

    Add an Example with the New API

---
+++ examples/StreamLogger.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of PMG\Queue
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+use Psr\Log\AbstractLogger;
+
+/**
+ * A logger implementation that ignores levels and just spits everything
+ * out to a stream.
+ *
+ * @since   2.0
+ */
+final class StreamLogger extends AbstractLogger
+{
+    private $stream;
+
+    public function __construct($stream=null)
+    {
+        $this->stream = $stream ?: fopen('php://stderr', 'w');
+    }
+
+    public function __destruct()
+    {
+        fclose($this->stream);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function log($level, $message, array $context=[])
+    {
+        $replace = $this->makeReplacements($context);
+        fwrite($this->stream, sprintf(
+            '[%s] %s%s',
+            $level,
+            strtr($message, $replace),
+            PHP_EOL
+        ));
+    }
+
+    private function makeReplacements(array $context)
+    {
+        $rv = [];
+        foreach ($context as $name => $replace) {
+            $rv[sprintf('{%s}', $name)] = $replace;
+        }
+
+        return $rv;
+    }
+}
