903417c80c9179bd95a688bb7c56dd5a316ddc34
Christopher Davis <cdavis9999@gmail.com>
2015-06-12 22:44:21 -0500

    Add a Pheanstalk Example

---
+++ examples/pheanstalk.php
@@ -0,0 +1,41 @@
+<?php
+
+use PMG\Queue;
+
+require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/StreamLogger.php';
+
+$driver = new Queue\Driver\PheanstalkDriver(new \Pheanstalk\Pheanstalk('localhost'));
+
+$router = new Queue\Router\SimpleRouter([
+    'TestMessage'   => 'q',
+    'TestMessage2'  => 'q',
+    'MustStop'      => 'q',
+]);
+
+$resolver = new Queue\Resolver\SimpleResolver([
+    'TestMessage'   => function () {
+        // noop
+    },
+    'TestMessage2'  => function () {
+        throw new \Exception('oops');
+    },
+    'MustStop'      => function () {
+        throw new Queue\Exception\SimpleMustStop('stopit');
+    },
+]);
+
+$producer = new Queue\DefaultProducer($driver, $router);
+
+$consumer = new Queue\DefaultConsumer(
+    $driver,
+    new Queue\Executor\SimpleExecutor($resolver),
+    new Queue\Retry\LimitedSpec(2), // allow two retries
+    new StreamLogger()
+);
+
+$producer->send(new Queue\SimpleMessage('TestMessage'));
+$producer->send(new Queue\SimpleMessage('TestMessage2'));
+$producer->send(new Queue\SimpleMessage('MustStop'));
+
+$consumer->run('q');
