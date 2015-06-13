4f0e262582282c9b19728109106306a081d50e17
Christopher Davis <cdavis9999@gmail.com>
2015-06-13 18:48:39 -0500

    Introduce the FallbackRouter

---
+++ src/Router/FallbackRouter.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of PMG\Queue
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace PMG\Queue\Router;
+
+use PMG\Queue\Message;
+use PMG\Queue\Router;
+
+/**
+ * A decorator that wraps another router and returns a fallback queue
+ * name if one isn't found.
+ *
+ * @since   2.0
+ */
+final class FallbackRouter implements \PMG\Queue\Router
+{
+    /**
+     * @var Router
+     */
+    private $wrapped;
+
+    /**
+     * @var string
+     */
+    private $fallbackQueue;
+
+    public function __construct(Router $wrapped, $fallbackQueue)
+    {
+        $this->wrapped = $wrapped;
+        $this->fallbackQueue = $fallbackQueue;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function queueFor(Message $message)
+    {
+        return $this->wrapped->queueFor($message) ?: $this->fallbackQueue;
+    }
+}
---
+++ test/unit/Router/FallbackRouterTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of PMG\Queue
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace PMG\Queue\Router;
+
+use PMG\Queue\Router;
+use PMG\Queue\SimpleMessage;
+
+class FallbackRouterTest extends \PMG\Queue\UnitTestCase
+{
+    const FALLBACK = 'FallbackQueue';
+
+    private $wrapped, $router, $message;
+
+    public function testQueueForReturnsValueFromWrappedRouterWhenFound()
+    {
+        $this->wrappedRouterReturns('FoundQueue');
+
+        $this->assertEquals('FoundQueue', $this->router->queueFor($this->message));
+    }
+
+    public function testQueueForReturnsFallbackValueWhenWrappedWRouterFindsNothing()
+    {
+        $this->wrappedRouterReturns(null);
+
+        $this->assertEquals(self::FALLBACK, $this->router->queueFor($this->message));
+    }
+
+    protected function setUp()
+    {
+        $this->wrapped = $this->getMock(Router::class);
+        $this->router = new FallbackRouter($this->wrapped, self::FALLBACK);
+        $this->message = new SimpleMessage('test');
+    }
+
+    private function wrappedRouterReturns($value)
+    {
+        $this->wrapped->expects($this->once())
+            ->method('queueFor')
+            ->with($this->identicalTo($this->message))
+            ->willReturn($value);
+    }
+}
