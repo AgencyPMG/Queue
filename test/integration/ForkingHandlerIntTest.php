8dd46ad0b63e3de3a4747ed682cdcf91a4569f35
Christopher Davis <cdavis9999@gmail.com>
2017-10-04 15:34:34 -0500

    Fix a Typo in DefaultConsumer
    And also add an integration test with the `PcntlForkingHandler` to
    verify that this fix works (and never happens again).

---
+++ test/integration/ForkingHandlerIntTest.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of PMG\Queue
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/Apache-2.0 Apache-2.0
+ */
+
+namespace PMG\Queue;
+
+/**
+ * @requires function pcntl_async_signals
+ */
+class ForkingHandlerIntTest extends IntegrationTestCase
+{
+    private $driver, $producer;
+
+    /**
+     * @group slow
+     */
+    public function testMessagesCanBeCancelledWhileInProgressFromConsumerStop()
+    {
+        $this->producer->send(new SimpleMessage('TestMessage'));
+        $consumer = $this->createConsumer(function () {
+            sleep(100);
+        });
+        pcntl_signal(SIGALRM, function () use ($consumer) {
+            $consumer->stop(1);
+        });
+        pcntl_alarm(5);
+
+        $exitCode = $consumer->run('q');
+
+        $this->assertSame(1, $exitCode);
+    }
+
+    protected function setUp()
+    {
+        pcntl_signal(SIGALRM, SIG_DFL);
+        pcntl_async_signals(true);
+        $this->driver = new Driver\MemoryDriver();
+
+        $this->producer = new DefaultProducer(
+            $this->driver,
+            new Router\SimpleRouter('q')
+        );
+    }
+
+    private function createConsumer(callable $handler) : DefaultConsumer
+    {
+        return new DefaultConsumer(
+            $this->driver,
+            new Handler\PcntlForkingHandler(new Handler\CallableHandler($handler)),
+            new Retry\NeverSpec()
+        );
+    }
+}
