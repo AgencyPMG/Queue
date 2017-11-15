d5bc23ec01756726ef63ae5f04a2b6a95d068c0f
Christopher Davis <cdavis9999@gmail.com>
2017-11-15 10:27:30 -0600

    Introduce DelegatingLifecycle
    Can be used to compose multiple lifecycles into one.

---
+++ test/unit/Lifecycle/LifecycleTestCase.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of PMG\Queue
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/Apache-2.0 Apache-2.0
+ */
+
+namespace PMG\Queue\Lifecycle;
+
+use PMG\Queue\MessageLifecycle;
+use PMG\Queue\Consumer;
+use PMG\Queue\SimpleMessage;
+
+abstract class LifecycleTestCase extends \PMG\Queue\UnitTestCase
+{
+    protected $consumer, $message;
+
+    protected function setUp()
+    {
+        $this->consumer = $this->createMock(Consumer::class);
+        $this->message = new SimpleMessage('example');
+    }
+
+    protected function mockLifecycle() : MessageLifecycle
+    {
+        return $this->createMock(MessageLifecycle::class);
+    }
+
+    protected function isConsumer()
+    {
+        return $this->identicalTo($this->consumer);
+    }
+
+    protected function isMessage()
+    {
+        return $this->identicalTo($this->message);
+    }
+}
